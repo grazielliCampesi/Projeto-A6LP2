73995ffdb27571dc959e5a8c1fc4a963123cc297
Grazi <graziellicampesi@gmail.com>
2017-05-20 18:22:17 -0300

    Primeiro Commit

---
+++ TrabalhoFinal/TrabalhoFinal/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Paisagens deslumbrantes";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Visite os Lugares ecologicos mais incriveis.";
+
+            return View();
+        }
+    }
+}
